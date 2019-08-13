ccba9c3a346cab562de45475a91848805f47cd8f
MASTERZ <soulhunz@hotmail.com>
2019-08-13 09:19:12 +0700

    Sign Out Finish

---
+++ lib/screens/my_constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+  final String nameApp = "การเดินทาง";
+  final Color textColor = Colors.blue;
+  final double h1 = 24.0;
+  final double h2 = 18.0;
+}
--- lib/screens/my_service.dart
+++ lib/screens/my_service.dart
@@ -1,5 +1,6 @@
+import 'package:arm_project/screens/home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 
 class MyService extends StatefulWidget {
   @override
@@ -7,6 +8,104 @@ class MyService extends StatefulWidget {
 }
 
 class _MyServiceState extends State<MyService> {
+  // Explicit
+  Color textColor = Color.fromARGB(0xFF, 0x00, 0x3c, 0x8f);
+  String nameLogin = '';
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  // Methods
+
+  @override
+  void initState() {
+    super.initState();
+    findDisplayName();
+  }
+
+  Future<void> findDisplayName() async {
+    FirebaseUser firebaseUser = await firebaseAuth.currentUser();
+    setState(() {
+      nameLogin = firebaseUser.displayName;
+    });
+  }
+
+  Future<void> signOutProcess() async {
+    await firebaseAuth.signOut().then((response) {
+      MaterialPageRoute materialPageRoute =
+          MaterialPageRoute(builder: (BuildContext context) => Home());
+
+      Navigator.of(context).pushAndRemoveUntil(
+          materialPageRoute, (Route<dynamic> route) => false);
+    });
+  }
+
+  Widget showLogo() {
+    return Container(
+      height: 80.0,
+      child: Image.asset(
+        'images/Video-logo.png',
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+
+  Widget showName() {
+    return Text(
+      'การเดินทาง',
+      style: TextStyle(
+          fontSize: 24.0,
+          color: textColor,
+          fontFamily: 'kanit',
+          fontStyle: FontStyle.italic),
+    );
+  }
+
+  Widget showLogin() {
+    return Text(
+      'Login $nameLogin',
+      style: TextStyle(
+        fontSize: 14.0,
+        fontStyle: FontStyle.italic,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  Widget myDrawerMenu() {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          myDrawerHead(),
+          signOutMenu(),
+        ],
+      ),
+    );
+  }
+
+  Widget myDrawerHead() {
+    return DrawerHeader(
+      decoration: BoxDecoration(
+        color: Colors.red,
+      ),
+      child: Column(
+        children: <Widget>[showLogo(), showName(), showLogin()],
+      ),
+    );
+  }
+
+  Widget signOutMenu() {
+    return ListTile(
+      leading: Icon(
+        Icons.android,
+        size: 36.0,
+      ),
+      title: Text("Sign Out"),
+      onTap: () {
+        signOutProcess();
+        Navigator.of(context).pop();
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +113,7 @@ class _MyServiceState extends State<MyService> {
         title: Text('My Service'),
       ),
       body: Text('My Body'),
+      drawer: myDrawerMenu(),
     );
   }
 }
